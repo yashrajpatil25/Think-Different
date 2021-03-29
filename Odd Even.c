56b393fe2fb8c8c480d229d7655528e07f08f792
yashrajpatil25 <81549201+yashrajpatil25@users.noreply.github.com>
2021-03-29 12:12:51 +0530

    Add files via upload

---
+++ Odd Even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a Number\n",a);
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+	printf("%d is Even\n",a);
+	}
+	else
+	{
+		printf("%d is Odd\n",a);
+	}
+	
+}
